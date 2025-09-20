4319d357885936f91090fc9dc1081831ec42d423
Edwin Wang <edwinwang62@gmail.com>
2025-09-20 16:20:52 +0300

    Updated

---
+++ script.js
@@ -0,0 +1,18 @@
+const images = document.querySelectorAll("#card img");
+let current = 0;
+
+function updateStack() {
+  images.forEach((img, i) => {
+    const indexFromCurrent = (i - current + images.length) % images.length;
+    img.style.zIndex = indexFromCurrent;
+    img.style.transform = `scale(${1 - indexFromCurrent * 0.03})`;
+  });
+
+  current = (current + 1) % images.length;
+}
+
+// Initialize stack
+updateStack();
+
+// Auto cycle every 3 seconds
+setInterval(updateStack, 1000);
